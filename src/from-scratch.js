c011f212840795623315799154eac3a0cac8d186
delgadilloabel1-cmd <delgadilloabel1@gmail.com>
2025-10-15 18:39:03 -0400

    completed from scratch

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -7,7 +7,7 @@ const addToFrontOrBack = (arr, value, isFront) => {
 };
 
 const reverseString = (string) => {
-  const array = string.split("")
+  const array = string.split("");
   return array.reverse().join("");
 };
 
@@ -24,16 +24,31 @@ const insertIntoMiddle = (arr, value) => {
 
 const deleteFromMiddle = (arr) => {
   const middleOfIndex = Math.floor(arr.length / 2);
-  arr.splice(middleOfIndex, 1)
+  arr.splice(middleOfIndex, 1);
 };
 
 const isRightIndex = (arr, value, index) => {
+  if (value === arr[index]) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-const roundAllNumsDown = () => {
+const roundAllNumsDown = (arr) => {
+  const newArray = [...arr];
+  for (let i = 0; i < arr.length; i++) {
+    newArray[i] = Math.floor(newArray[i])
+  }
+  return newArray
 };
 
-const getAllYCoordinates = () => {
+const getAllYCoordinates = (arrOfCoords) => {
+  const newArray = [];
+  for (let i = 0; i < arrOfCoords.length; i++) {
+    newArray.push(arrOfCoords[i][1]);
+  }
+  return newArray
 };
 
 module.exports = {
